ab17ced217283d289b60a91cc05350434556b7dc
Torleif Nielsen <torleif@hawaii.edu>
2016-03-03 12:32:47 -1000

    Set up additional assets in the Products collection.

--- fridge-copycat-meteor.js
+++ fridge-copycat-meteor.js
@@ -1,18 +1,19 @@
 Products = new Mongo.Collection('products');
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+  Template.fridge.helpers({
+    products: function () {
+      return Products.find({
+        place: 'fridge'
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.productList.helpers({
+    products: function () {
+      return Products.find({
+        place: 'supermarket'
+      });
     }
   });
 }
@@ -20,5 +21,30 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Products.remove({});
+
+    Products.insert({
+      name: 'Milk',
+      img: '/milk.png',
+      place: 'fridge'
+    });
+
+    Products.insert({
+      name: 'Bread',
+      img: '/bread.png',
+      place: 'supermarket'
+    });
+
+    Products.insert({
+      name: 'Banana',
+      img: '/banana.png',
+      place: 'fridge'
+    });
+
+    Products.insert({
+      name: 'Juice',
+      img: '/juice.png',
+      place: 'fridge'
+    });
   });
 }
